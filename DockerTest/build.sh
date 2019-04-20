d34aaea3770438a5360178d01874274fe6c27d47
孙红军 <16414766@qq.com>
2019-04-20 12:03:35 +0800

    1

--- DockerTest/build.sh
+++ DockerTest/build.sh
@@ -1,1 +1,13 @@
-﻿cd /Users
+﻿
+cd /Users/sunhongjun/Project/jenkins/workspace/DOTNETCORE_CI_TEST
+cp DockerTest/Dockerfile .
+
+#build
+docker build -t docker-dotnetcore-testapp:$BUILD_NUMBER .
+
+# stop&delete current container
+docker stop $(docker ps -a | grep dotnet-testapp | awk '{print $1}')
+docker rm $(docker ps -a | grep dotnet-testapp | awk '{print $1}')
+
+#run the new build
+docker run --name dotnet-testapp -d -p 8810:8810 docker-dotnetcore-testapp:1.0
